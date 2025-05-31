95b569ba03a1c3314893a0219cc749b488e97673
tuchango <lapindanil22@gmail.com>
2025-05-31 18:16:05 +0300

    add read methods in DAO-layer

--- app/dao.py
+++ app/dao.py
@@ -1,12 +1,35 @@
+import sqlite3
 from app.models import Currency, ExchangeRate
+
+DB_PATH = "../currencies.db"
+
+
+def _get_conn():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    # conn.execute("PRAGMA foreign_keys = ON")
+    return conn
 
 
 def get_all_curencies() -> list[Currency]:
-    pass
+    conn = _get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT code, full_name FROM Currencies")
+    rows = cur.fetchall()
+    conn.close()
+    return [Currency(row["code"], row["full_name"]) for row in rows]
 
 
 def get_currency_by_code(code: str) -> Currency | None:
-    pass
+    conn = _get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT code, full_name FROM Currencies WHERE code = ?", (code,))
+    row = cur.fetchone()
+    conn.close()
+    
+    if row:
+        return Currency(row["code"], row["full_name"])
+    return None
 
 
 def add_currency(Currency) -> None:
@@ -14,12 +37,25 @@ def add_currency(Currency) -> None:
 
 
 def get_all_exchange_rates() -> list[ExchangeRate]:
-    pass
+    conn = _get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT base_curr, target_curr, rate FROM ExchangeRates")
+    rows = cur.fetchall()
+    conn.close()
+    return [ExchangeRate(row["base_curr"], row["target_curr"], row["rate"]) for row in rows]
 
 
 def add_exchange_rate(Rate) -> None:
     pass
 
 
-def get_rate(from_code, to_code) -> float:
-    pass
+def get_exchange_rate(from_code, to_code) -> float | None:
+    conn = _get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT rate FROM ExchangeRates WHERE base_curr = ? AND target_curr = ?", (from_code, to_code))
+    row = cur.fetchone()
+    conn.close()
+    
+    if row:
+        return row["rate"]
+    return None
